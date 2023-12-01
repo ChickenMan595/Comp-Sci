486180173a304ac66959c348e62388492e1ce8a7
ChickenMan595 <152421182+ChickenMan595@users.noreply.github.com>
2023-12-01 11:51:48 -0500

    Add files via upload

--- Zombie Defense.py
+++ Zombie Defense.py
@@ -5,17 +5,29 @@ Created on Mon Nov 27 10:57:55 2023
 @author: pclee
 """
 
-import pygame, simpleGE
+import pygame, simpleGE, random
     
 pygame.init()
 
-class archer(simpleGE.SuperSprite):
+class Tower(simpleGE.SuperSprite):
     def __init__(self, scene):
         super().__init__(scene)
-        self.setImage("Archer.png")
+        self.setImage("tower2.png")
+        self.setSize(120, 460)
+        self.y = (270)
+        self.x = (115)
+        
+    def setPosition (self, position):
+        (self.x, self.y) = position
+        self.rect.center = self.x, self.y
+        
+class Archer(simpleGE.SuperSprite):
+    def __init__(self, scene):
+        super().__init__(scene)
+        self.setImage("Archer_ready.png")
         self.setSize(50, 50)
-        self.y = (185)
-        self.x = (150)
+        self.y = (170)
+        self.x = (200)
         
     def checkEvents(self):
         super().checkEvents()
@@ -28,8 +40,29 @@ class archer(simpleGE.SuperSprite):
     def setPosition (self, position):
         (self.x, self.y) = position
         self.rect.center = self.x, self.y
+        
+class Lizard(simpleGE.SuperSprite):
+    def __init__(self, scene):
+        super().__init__(scene)
+        self.setImage("Lizard.png")
+        self.setSize(50, 50)
+        self.y = random.randint(180, 400)
+        self.x = 660
+        self.speed = random.randint(-6, -2)
     
-class arrow(simpleGE.SuperSprite):
+    def checkEvents(self):
+        if self.collidesWith(self.scene.tower):
+            self.reset()
+        if self.collidesWith(self.scene.arrow):
+            self.reset()
+        
+    def reset(self):
+        self.y = random.randint(180, 400)
+        self.x = 640
+        self.dx = random.randint(-6, -2)
+
+        
+class Arrow(simpleGE.SuperSprite):
     def __init__(self, scene, parent):
         super().__init__(scene)
         self.parent = parent
@@ -47,8 +80,8 @@ class arrow(simpleGE.SuperSprite):
 class Crosshair(simpleGE.BasicSprite):
     def __init__(self, scene):
         super().__init__(scene)
-        self.setImage("cross_normal.png")
-        self.setSize(10, 10)
+        self.setImage("crosshair6.png")
+        self.setSize(50, 50)
 
     def checkEvents(self):
         (mx, my) = pygame.mouse.get_pos()
@@ -59,23 +92,34 @@ class Game(simpleGE.Scene):
     def __init__(self):
         super().__init__()
         self.setCaption("Arrow keys to aim")
-        self.archer = archer(self)
+        self.archer = Archer(self)
         self.NUM_ARROW = 100
         self.currentArrow = 0       
         self.arrow = []
         for i in range(self.NUM_ARROW):
-            self.arrow.append(arrow(self, self.archer))
+            self.arrow.append(Arrow(self, self.archer))
+            
+        self.lizard = []
+        for i in range(7):
+            self.lizard.append(Lizard(self))
                                 
-        self.sprites = [self.archer, self.arrow]
-        self.background = pygame.image.load("forest_background.png")
+        self.background = pygame.image.load("Defense_Background.png")
         self.background = pygame.transform.scale(self.background, (640,480))
+
+        pygame.mouse.set_visible(False)
+        
+        self.crosshair = Crosshair(self)
+        self.tower = Tower(self)
+
+        self.sprites = [self.archer, self.arrow, self.tower, self.crosshair, self. lizard]
+
 
     def doEvents(self, event):
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 self.currentArrow += 1
                 if self.currentArrow >= self.NUM_ARROWS:
-                    self.currentBullet = 0
+                    self.currentArrow = 0
                 self.arrow[self.currentArrow].fire()
                 
     
